e73f19c0cf4c3684f268d53a2a62e05ea6de3c4f
fayeefe <freija.orvokki@gmail.com>
2025-01-17 17:28:43 +0200

    version1

--- task2/src/main/java/main/App.java
+++ task2/src/main/java/main/App.java
@@ -4,17 +4,6 @@ import java.util.Scanner;
 
 public class App {
 
-    public static void start(){
-        Car car  = new Car();
-        car.status();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Anna auton merkki: ");
-        car.make = scanner.next();
-        System.out.println("Anna auton malli: ");
-        car.model = scanner.next();
-        System.out.println("Anna auton nopeus: ");
-        car.speed = scanner.nextInt();
-    }
     public static void menuPrint(){
         System.out.println("1) Näytä auton tila");
         System.out.println("2) Muokkaa auton merkkiä ja mallia");
@@ -26,19 +15,18 @@ public class App {
     public static void main(String[] args) {
         boolean cont = true;
         Car car  = new Car();
-        start();
-        /*car.status();
+        car.status();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Anna auton merkki: ");
         car.make = scanner.next();
         System.out.println("Anna auton malli: ");
         car.model = scanner.next();
         System.out.println("Anna auton nopeus: ");
-        car.speed = scanner.nextInt(); */
+        car.speed = scanner.nextInt();
 
         while (cont == true) {
             menuPrint();
-            switch (start().) {
+            switch (scanner.nextInt()) {
                 case 1:
                     car.status();
                     break;
